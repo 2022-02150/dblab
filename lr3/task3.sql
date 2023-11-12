07abdbb6095599fe10a404da37fdfdc784d2ec3e
Sophia <zi.sony@yandex.ru>
2023-11-12 17:05:44 +0300

    lr3

---
+++ lr3/task3.sql
@@ -0,0 +1,5 @@
+USE cd;
+/* Выберите членов клуба, которые рекомендовали других членов для вступления. */
+SELECT surname, firstname 
+FROM members
+WHERE memid IN (SELECT recommendedby FROM members WHERE recommendedby IS NOT NULL);
