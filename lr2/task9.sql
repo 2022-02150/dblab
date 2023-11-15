29c877aff9f1ec2774d1c6cb3dc631715909b420
Sophia <zi.sony@yandex.ru>
2023-11-15 15:03:45 +0300

    10

--- lr2/task9.sql
+++ lr2/task9.sql
@@ -1,3 +1,3 @@
 /* Выбрать всех членов клуба, зарегистрированных с сентября 2012 года.*/
 USE cd;
-SELECT memid, surname, firstname  FROM members WHERE joindate >= '2012-09-01';
+SELECT memid, surname, firstname, JOINDATE  FROM members  WHERE joindate > '2012-07-01' AND joindate < '2012-08-01';
