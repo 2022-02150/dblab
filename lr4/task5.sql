62ab5144639ae1709409904c2f1d1031f6e99464
Sophia <zi.sony@yandex.ru>
2023-11-19 23:34:48 +0300

    lr

---
+++ lr4/task5.sql
@@ -0,0 +1,6 @@
+/*Стоимость аренды объектов 0 и 1 изменилась (член клуба == 6, гость = 30). Обновите данные в таблицах.*/
+USE cd;
+UPDATE facilities 
+set membercost = 6, guestcost = 30 
+where facid = 1 or facid = 0;
+SELECT * FROM facilities;
